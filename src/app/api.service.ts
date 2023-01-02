3d59cca3aeca7fbfa7a1f2d94845d1d9a831f6ea
umbreon222 <umbreon222@gmail.com>
2023-01-02 01:27:42 -0600

    Just use environment.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { PumpState } from 'src/models/pump-state';
 import { Settings } from 'src/models/settings';
 
-const BASE_URL = 'http://127.0.0.1:7362/';
 const SETTINGS_ENDPOINT = 'settings/';
 const PUMPS_ENDPOINT = 'pumps/';
 
@@ -12,25 +12,29 @@ const PUMPS_ENDPOINT = 'pumps/';
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClient) {}
+  private apiBaseUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiBaseUrl = environment.apiBaseUrl;
+  }
 
   public getSettings(): Observable<Settings> {
-    const url = BASE_URL + SETTINGS_ENDPOINT;
+    const url = this.apiBaseUrl + SETTINGS_ENDPOINT;
     return this.getJson<Settings>(url);
   }
   
   public putSettings(settings: Settings): Observable<any> {
-    const url = BASE_URL + SETTINGS_ENDPOINT;
+    const url = this.apiBaseUrl + SETTINGS_ENDPOINT;
     return this.putJson(url, JSON.stringify(settings));
   }
 
   public getPumpStates(): Observable<PumpState[]> {
-    const url = BASE_URL + PUMPS_ENDPOINT;
+    const url = this.apiBaseUrl + PUMPS_ENDPOINT;
     return this.getJson<PumpState[]>(url);
   }
 
   public postPump(pumpNumber: number, duration: number): Observable<any> {
-    const url = BASE_URL + PUMPS_ENDPOINT + pumpNumber.toString();
+    const url = this.apiBaseUrl + PUMPS_ENDPOINT + pumpNumber.toString();
     return this.postText(url, duration.toString());
   }
 
